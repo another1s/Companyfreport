af5a5bc5565d33f3bb624a3050835a61172a27da
another1s <164536681@qq.com>
2019-07-09 00:55:25 +0800

    update jeff required version

---
+++ jeff_required.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+import time
+
+ADDRESS = 'https://www.hkexnews.hk/index.htm'
+
+
+def greedy_digging(doppler):
+    try:
+        loadmore = doppler.find_element_by_xpath('//*[@id="recordCountPanel2"]/div[1]/div/div[1]/ul/li/a')
+        if loadmore:
+            doppler.execute_script("arguments[0].click();", loadmore)
+            greedy_digging(doppler)
+    except:
+        print("nothing more")
+
+
+search_keys = '00001'
+browser = webdriver.Chrome()
+browser.get(ADDRESS)
+searchbar = browser.find_element_by_xpath('//*[@id="searchStockCode"]').send_keys(search_keys)
+time.sleep(3)
+listbox = browser.find_element_by_xpath('//*[@id="autocomplete-list-0"]/div[1]/div[1]/table/tbody/tr[1]/td[2]/span').click()
+time.sleep(3)
+
+openlist = browser.find_element_by_xpath('//*[@id="tier1-select"]/div/div/a').click()
+time.sleep(3)
+test = browser.find_element_by_xpath('//*[@id="tier1-select"]/div/div/a')
+
+filetype = browser.find_element_by_link_text('Headline Category').click()
+time.sleep(3)
+confirmbutton = browser.find_element_by_xpath('//*[@id="tab-panel-title-search"]/form/div/div[3]/a[3]').click()
+time.sleep(3)
+
+financial_statement = browser.find_element_by_xpath('//*[@id="rbAfter2006"]/div[2]/div/div/ul/li[5]/a').click()
+time.sleep(2)
+fetch_all = browser.find_element_by_xpath('//*[@id="rbAfter2006"]/div[2]/div/div/ul/li[5]/div/ul/li[1]/a').click()
+time.sleep(2)
+greedy_digging(browser)
+time.sleep(3)
+#while (browser.find_element_by_xpath('//*[@id="recordCountPanel2"]/div[1]/div/div[2]')):
+#	browser.find_element_by_xpath('//*[@id="recordCountPanel2"]/div[1]/div/div[2]').click()
+#	time.sleep(2)
+
+
+html = browser.page_source
+
+
+soup = BeautifulSoup(html, 'lxml')
+pdf_files = soup.select('div[class="doc-link"]')
+
+pdf_url_list = list()
+
+print(type(pdf_files))
+for f in pdf_files:
+    print(type(f))
+    print('\n')
+    print(f.a['href'])
+    pdf_url_list.append(f.a['href'])
+    #pdf_url = f.find_next_sibling('a')
+    #print(pdf_url)
+
+with open("长和.txt", 'w') as f:
+    for pdf in pdf_url_list:
+        prefix = "https://www1.hkexnews.hk"
+        f.write(prefix+pdf)
+        f.write('\n')
+
+#print(button)
