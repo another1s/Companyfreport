1d13a350430e96b5123f07ed7e80ffcc3573599b
unknown <hezheyu874@paxjk.com>
2019-07-12 08:59:31 +0800

    finished crawling financial statement url

--- jeff_required.py
+++ jeff_required.py
@@ -1,9 +1,17 @@
 from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
 import time
 
 ADDRESS = 'https://www.hkexnews.hk/index.htm'
+def read_code(filename):
+    res = list()
+    with open(filename, 'r',encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            #print(line)
+            #data['companyname'] = line_split[1]
+            res.append(line.strip('\n'))
+    return res
 
 
 def greedy_digging(doppler):
@@ -16,58 +24,62 @@ def greedy_digging(doppler):
         print("nothing more")
 
 
+def crazydiamond(search_key):
+    browser = webdriver.Chrome()
+    browser.get(ADDRESS)
+    # set up company name and code
+    searchbar = browser.find_element_by_xpath('//*[@id="searchStockCode"]').send_keys(search_key)
+    time.sleep(2)
+    listbox = browser.find_element_by_xpath('//*[@id="autocomplete-list-0"]/div[1]/div[1]/table/tbody/tr[1]/td[2]/span').click()
+    time.sleep(2)
+    # limiting range of the searching : focus on financial statement
+    openlist = browser.find_element_by_xpath('//*[@id="tier1-select"]/div/div/a').click()
+    time.sleep(2)
+    filetype = browser.find_element_by_link_text('Headline Category').click()
+    time.sleep(2)
+    opensublist = browser.find_element_by_xpath('//*[@id="rbAfter2006"]/div[1]/div/div/a').click()
+    time.sleep(1)
+    financial_statement = browser.find_element_by_link_text('Financial Statements/ESG Information').click()
+    time.sleep(1)
+    fetch_all = browser.find_elements_by_link_text('ALL')
+    target = fetch_all[2].click()
+    confirmbutton = browser.find_element_by_xpath('//*[@id="tab-panel-title-search"]/form/div/div[3]/a[3]').click()
+    time.sleep(1)
+    greedy_digging(browser)
+    time.sleep(1)
+    # while (browser.find_element_by_xpath('//*[@id="recordCountPanel2"]/div[1]/div/div[2]')):
+    #	browser.find_element_by_xpath('//*[@id="recordCountPanel2"]/div[1]/div/div[2]').click()
+    #	time.sleep(2)
+
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    pdf_files = soup.select('div[class="doc-link"]')
+    pdf_url_list = list()
+    #print(type(pdf_files))
+    for f in pdf_files:
+        #print(type(f))
+        #print('\n')
+        #print(f.a['href'])
+        pdf_url_list.append(f.a['href'])
+        # pdf_url = f.find_next_sibling('a')
+        # print(pdf_url)
+
+    with open('./fileurl/' + search_key + ".txt", 'w') as f:
+        for pdf in pdf_url_list:
+            prefix = "https://www1.hkexnews.hk"
+            f.write(prefix + pdf)
+            print(prefix+pdf)
+            f.write('\n')
+    browser.close()
+
+
 search_keys = '00001'
-browser = webdriver.Chrome()
-browser.get(ADDRESS)
 
-# set up company name and code
-searchbar = browser.find_element_by_xpath('//*[@id="searchStockCode"]').send_keys(search_keys)
-time.sleep(3)
-listbox = browser.find_element_by_xpath('//*[@id="autocomplete-list-0"]/div[1]/div[1]/table/tbody/tr[1]/td[2]/span').click()
-time.sleep(3)
-# limiting range of the searching : focus on financial statement
-openlist = browser.find_element_by_xpath('//*[@id="tier1-select"]/div/div/a').click()
-time.sleep(3)
-filetype = browser.find_element_by_link_text('Headline Category').click()
-time.sleep(3)
-opensublist = browser.find_element_by_xpath('//*[@id="rbAfter2006"]/div[1]/div/div/a').click()
-time.sleep(2)
-financial_statement = browser.find_element_by_link_text('Financial Statements/ESG Information').click()
-time.sleep(2)
-fetch_all = browser.find_elements_by_link_text('ALL')
-target = fetch_all[2].click()
-confirmbutton = browser.find_element_by_xpath('//*[@id="tab-panel-title-search"]/form/div/div[3]/a[3]').click()
-time.sleep(3)
+comp_info = read_code('companycode.txt')
 
-
-greedy_digging(browser)
-time.sleep(3)
-#while (browser.find_element_by_xpath('//*[@id="recordCountPanel2"]/div[1]/div/div[2]')):
-#	browser.find_element_by_xpath('//*[@id="recordCountPanel2"]/div[1]/div/div[2]').click()
-#	time.sleep(2)
-
-
-html = browser.page_source
-
-
-soup = BeautifulSoup(html, 'lxml')
-pdf_files = soup.select('div[class="doc-link"]')
-
-pdf_url_list = list()
-
-print(type(pdf_files))
-for f in pdf_files:
-    print(type(f))
-    print('\n')
-    print(f.a['href'])
-    pdf_url_list.append(f.a['href'])
-    #pdf_url = f.find_next_sibling('a')
-    #print(pdf_url)
-
-with open("长和财报.txt", 'w') as f:
-    for pdf in pdf_url_list:
-        prefix = "https://www1.hkexnews.hk"
-        f.write(prefix+pdf)
-        f.write('\n')
-
-#print(button)
+for i in comp_info:
+    print(i)
+    try:
+        crazydiamond(i)
+    except:
+        pass
